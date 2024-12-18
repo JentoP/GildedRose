039505fb251cc9b7a84ccd1aab30d0dfc61078e0
Jento <jento.pieters@gmail.com>
2024-12-18 20:57:55 +0100

    0

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,8 @@
 package com.gildedrose;
 
+/**
+ * @version 1.0
+ */
 class GildedRose {
     Item[] items;
     final int MAX_QUALITY = 50;
@@ -8,11 +11,19 @@ class GildedRose {
         this.items = items;
     }
 
+    /**
+     * @author Jento Pieters
+     * @description updates the complete list of items by using both updateSellInValue and updateQuality methods
+     */
     public void updateCatalog() {
         updateSellInValue();
         updateQuality();
     }
 
+    /**
+     * @author Jento Pieters
+     * @description updates the quality of the items ensuring that it meets the requirements.
+     */
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
 
@@ -25,6 +36,9 @@ class GildedRose {
 //            updates quality of Backstage passes
             if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality < MAX_QUALITY) {
+                    if (items[i].sellIn > 10) {
+                        items[i].quality = items[i].quality + 1;
+                    }
                     if (items[i].sellIn <= 10) {
                         items[i].quality = items[i].quality + 2;
                     }
@@ -53,6 +67,10 @@ class GildedRose {
         }
     }
 
+    /**
+     * @description updates the sellIn value of the items
+     *  it makes it possible to update the sellIn value of all items by 1 day
+     */
     public void updateSellInValue() {
         for (int i = 0; i < items.length; i++) {
             if (items[i].sellIn < MAX_QUALITY) {
@@ -61,60 +79,4 @@ class GildedRose {
         }
     }
 }
-
-//
-//        for(
-//    int i = 0;
-//    i<items.length;i++)
-//
-//    {
-//        if (!items[i].name.equals("Aged Brie")
-//                && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//            if (items[i].quality > 0) {
-//                if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-//                    items[i].quality = items[i].quality - 1;
-//                }
-//            }
-//        } else {
-//            if (items[i].quality < 50) {
-//                items[i].quality = items[i].quality + 1;
-//
-//                if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                    if (items[i].sellIn < 11) {
-//                        if (items[i].quality < 50) {
-//                            items[i].quality = items[i].quality + 1;
-//                        }
-//                    }
-//
-//                    if (items[i].sellIn < 6) {
-//                        if (items[i].quality < 50) {
-//                            items[i].quality = items[i].quality + 1;
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//
-//        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-//            items[i].sellIn = items[i].sellIn - 1;
-//        }
-//
-//        if (items[i].sellIn < 0) {
-//            if (!items[i].name.equals("Aged Brie")) {
-//                if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-//                    if (items[i].quality > 0) {
-//                        if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-//                            items[i].quality = items[i].quality - 1;
-//                        }
-//                    }
-//                } else {
-//                    items[i].quality = items[i].quality - items[i].quality;
-//                }
-//            } else {
-//                if (items[i].quality < 50) {
-//                    items[i].quality = items[i].quality + 1;
-//                }
-//            }
-//        }
-//    }
 
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -4,16 +4,69 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * @version 1.0
+ * @description test class for GildedRose
+ *
+ */
 class GildedRoseTest {
 
     @Test
-    void foo() {
+    void testNewCatalog() {
         Item[] items = new Item[]{new Item("foo", 0, 0)};
         GildedRose app = new GildedRose(items);
         app.updateQuality();
         assertEquals("foo", app.items[0].name);
     }
 
+    @Test
+    void testQualityAgedBrie() {
+        Item[] items = new Item[]{new Item("Aged Brie", 2, 0)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Aged Brie", app.items[0].name);
+        assertEquals(1, app.items[0].quality);
+        System.out.println("\n Name: " + app.items[0].name + "\n Quality: " + app.items[0].quality + "\n Sell in " + app.items[0].sellIn + " day(s)");
+    }
 
+    @Test
+    void testQualityBackstagePasses() {
+        Item[] items = new Item[]{new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Backstage passes to a TAFKAL80ETC concert", app.items[0].name);
+        assertEquals(21, app.items[0].quality);
+        System.out.println("\n Name: " + app.items[0].name + "\n Quality: " + app.items[0].quality + "\n Sell in " + app.items[0].sellIn + " day(s)");
+    }
 
+    @Test
+    void testQualityConjured() {
+        Item[] items = new Item[]{new Item("Conjured Mana Cake", 3, 6)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals("Conjured Mana Cake", app.items[0].name);
+        assertEquals(4, app.items[0].quality);
+        System.out.println("\n Name: " + app.items[0].name + "\n Quality: " + app.items[0].quality + "\n Sell in " + app.items[0].sellIn + " day(s)");
+    }
+
+    @Test
+    void testLegendaryItem() {
+
+        Item[] items = new Item[]{new Item("Sulfuras, Hand of Ragnaros", 0, 80)};
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        app.updateSellInValue();
+        assertEquals("Sulfuras, Hand of Ragnaros", app.items[0].name);
+        assertEquals(80, app.items[0].quality);
+        System.out.println("\n Name: " + app.items[0].name + "\n Quality: " + app.items[0].quality + "\n Sell in " + app.items[0].sellIn + " day(s)");
+    }
+
+    @Test
+    void testOnlySellInValue() {
+        Item[] items = new Item[]{new Item("Sword", 5, 5)};
+        GildedRose app = new GildedRose(items);
+        app.updateSellInValue();
+        assertEquals(4, app.items[0].sellIn);
+        System.out.println("\n Name: " + app.items[0].name + "\n Quality: " + app.items[0].quality + "\n Sell in " + app.items[0].sellIn + " day(s)");
+    }
 }
